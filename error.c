5e80db8dfed83a057de6c8b0165d9b7609680ded
JamesTeeters <4925@holbertonschool.com>
2022-08-10 14:25:24 -0700

    'added file that house error print functions'

---
+++ error.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * open_error - if the file can't be opened.
+ * @f: file
+ *
+ * Return: nothing.
+ */
+void open_error(char *f)
+{
+	fprintf(stderr, "Error: Can't open file $s\n", f);
+	exit(EXIT_FAILURE);
+}
+/**
+ * usage_error - if file doesnt exist or more than one argumnet.
+ *
+ * Return: nothing.
+ */
+void usage_error(void)
+{
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+}
