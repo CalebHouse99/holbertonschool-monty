1b810c0818071ee76915b6cda071104eabb86c62
CalebHouse99 <calebhouse99@gmail.com>
2022-08-10 15:10:59 -0700

    snap crackle pop!

--- pop.c
+++ pop.c
@@ -2,12 +2,20 @@
 
 /**
  * pop - removes from the top of the stack
- * 
+ *
  * @stack: pointer to head node
- * @line_number: current line number 
+ * @line_number: current line number
  */
 
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp = *stack;
 
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+	free(temp);
 }
