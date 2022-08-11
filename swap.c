b07f3fbad1b29155073312ca6e15df7fd02d4321
JamesTeeters <4925@holbertonschool.com>
2022-08-11 12:02:59 -0700

    'added function to swap top 2 elements of stack'

--- swap.c
+++ swap.c
@@ -9,9 +9,16 @@
 
 void swap(stack_t **stack, unsigned int line_number)
 {
+	int tmp;
+
 	if ((*stack)->next == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
+	tmp = (*stack)->next->n;
+	(*stack)->next->n = (*stack)->n;
+	(*stack)->n = tmp;
+
 }
