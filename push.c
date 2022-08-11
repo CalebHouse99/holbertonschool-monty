bd70fa67454da1fe4baf12836897b4bd3ddf07d6
CalebHouse99 <calebhouse99@gmail.com>
2022-08-11 12:58:34 -0700

    error message correction

--- push.c
+++ push.c
@@ -18,7 +18,7 @@ void push(stack_t **stack, unsigned int line_number)
 
 	if (arg == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
