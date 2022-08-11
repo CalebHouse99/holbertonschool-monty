8b512b2bd38d291122a83d83e060bc73d943ce0d
CalebHouse99 <calebhouse99@gmail.com>
2022-08-11 11:36:59 -0700

    edge case if null

--- pall.c
+++ pall.c
@@ -12,6 +12,9 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	(void) line_number;
 
+	if (stack == NULL || *stack == NULL)
+		return;
+
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
