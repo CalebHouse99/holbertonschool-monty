d2b48102ddd5c01c93018cef6efebf5a251cfc08
CalebHouse99 <calebhouse99@gmail.com>
2022-08-11 10:10:25 -0700

    push works now, m files fixed

--- push.c
+++ push.c
@@ -10,26 +10,32 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
+	char *arg = Arg.argument;
+	int i;
+	int convert;
 	stack_t *new = NULL;
 	(void) line_number;
 
-	new = malloc(sizeof(stack_t));
-
-	if (new == NULL)
+	if (arg == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-		free(new);
 		exit(EXIT_FAILURE);
 	}
 
-	if (*stack == NULL)
+	for (i = 0; arg[i] != '\0'; i++)
 	{
-		new->next = NULL;
+		if ((isdigit(arg[i])) == 0 && arg[i] != '-')
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);	
+		}
 	}
-	else
-	{
-		(*stack)->prev = new;
-		new->prev = NULL;
-		new->next = *stack;
-	}
+	
+	convert = atoi(arg);
+	new = malloc(sizeof(stack_t));
+
+	new->n = convert;
+	new->prev = NULL;
+	new->next = *stack;
+	*stack = new;
 }
